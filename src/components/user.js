d191c3226ef912d1a4bb0cf3136d347a7754bb58
taiyi <god830311@gmail.com>
2021-05-31 19:04:16 +0800

    安裝lodash

--- src/components/user.js
+++ src/components/user.js
@@ -1,65 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import { getBalance, getAccount } from '../api'
+import _ from 'lodash'
 
 const User = () => {
   const [balance, setBalance] = useState()
   const [account, setAccount] = useState()
 
-  
-
-  useEffect(()=>{
-    const getBalanceData = async() => {
+  useEffect(() => {
+    const getBalanceData = async () => {
       const balanceData = await getBalance()
       setBalance(balanceData)
     }
     getBalanceData()
-  },[])
+  }, [])
 
-  useEffect(()=>{
-    const getAccountData = async() => {
+  useEffect(() => {
+    const getAccountData = async () => {
       const accountData = await getAccount()
       setAccount(accountData)
     }
     getAccountData()
-  },[])
+  }, [])
 
   return (
     <div className="space-y-2">
       <div className="flex items-center">
         <span className="text-red-600 text-lg mr-5 font-bold">使用者帳號資訊:</span>
       </div>
-      
+
       <div className="flex items-center">
-        <span className="text-white text-lg mr-5 font-bold">測試用餘額:{balance.USD.free}</span>
-        {console.log(balance)}
-        
+        <span className="text-white text-lg mr-5 font-bold">測試用餘額:{_.get(balance, 'USD.free', 0)}</span>
       </div>
 
       <div className="flex items-center">
         <span className="text-white text-lg mr-5 font-bold">帳號名稱:</span>
       </div>
-      
+
       <div className="flex items-center">
         <span className="text-white text-lg mr-5 font-bold">盈虧%數:</span>
-        
       </div>
 
       <div className="flex items-center">
         <span className="text-white text-lg mr-5 font-bold">盈虧金額:</span>
-        
       </div>
 
       <div className="flex items-center">
         <span className="text-white text-lg mr-5 font-bold">已開倉%數:</span>
-        
       </div>
-      
+
       <div className="flex items-center">
         <span className="text-white text-lg mr-5 font-bold">本週獲利:</span>
-        
       </div>
-      
-      
     </div>
   )
 }
