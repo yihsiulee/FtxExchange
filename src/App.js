d07e326f61aacff391edd4110660fb2d510ebb00
taiyi <god830311@gmail.com>
2021-06-01 22:18:57 +0800

    修理slide bar初始值問題

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { REACT_APP_USER1_APIKEY, REACT_APP_USER1_SECRET } from './config'
-import { getMarkets, getTicker, changeLeverage, getAccount} from './api'
+import { getMarkets, getTicker, getAccount } from './api'
 import { useSelectStyles } from './styles'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
@@ -13,11 +13,11 @@ import Typography from '@material-ui/core/Typography'
 import Slider from '@material-ui/core/Slider'
 import _ from 'lodash'
 function App() {
-  console.log('user1:', REACT_APP_USER1_APIKEY, REACT_APP_USER1_SECRET)
+  // console.log('user1:', REACT_APP_USER1_APIKEY, REACT_APP_USER1_SECRET)
   const [markets, setMarkets] = useState({}) //市場上所有的幣別
   const [symbol, setSymbol] = useState('') // symbol代表幣別 e.g. ETH/BTC, LTC/BTC
   const [ticker, setTicker] = useState({})
-  const [slideValue, setSlideValue] = useState('10')
+  const [slideValue, setSlideValue] = useState(10)
   const [account, setAccount] = useState({})
   const leverageMarks = [
     {
@@ -54,31 +54,21 @@ function App() {
     getTickerData()
   }, [symbol])
 
-    
-    
- 
-
   //調整槓桿倍率
   const handleChangeSlide = (event, newValue) => {
     setSlideValue(newValue)
-    // changeLeverage(slideValue)
   }
-  
+
   //選幣別時,把選項存起來,底線是他會傳兩個參數,可是只用的到第二個,第一格就可以放底線
   const handleChangeSymbol = (_, value) => {
     setSymbol(value?.id)
   }
-
   return (
-    
-    <div className="h-screen w-screen flex  bg-darkblue">
+    <div className="h-full w-full flex  bg-darkblue">
       <div className="w-1/2  justify-items-center rounded-xl p-4 space-y-2 m-auto bg-lightblue">
         <div className="flex items-center">
           <span className="text-white text-lg mr-5 font-bold">獲取時間:</span>
           <span className="text-white">{moment(parseInt(ticker?.timestamp)).format('YYYY-MM-DD HH:mm:ss')}</span>
-          {console.log(account)}
-
-        
         </div>
 
         <div className="flex items-center">
@@ -102,25 +92,25 @@ function App() {
         </div>
 
         <div className="flex items-center">
-        <span className="text-white text-lg mr-5 font-bold">
-          <div className="flex items-center">
-            <span className="mr-5">槓桿倍數:</span>
-            <Typography id="discrete-slider-custom">{`leverage: ${slideValue}x`}</Typography>
-          </div>
-          <div>
-            {console.log("L:",_.get(account,'result.leverage',3),"X;",slideValue)}
-            <Slider
-              style={{ width: 300 }}
-              defaultValue={10}
-              onChange={handleChangeSlide}
-              aria-labelledby="discrete-slider-custom"
-              step={1}
-              valueLabelDisplay="auto"
-              marks={leverageMarks}
-            />
-          </div>
-        </span>
-      </div>
+          <span className="text-white text-lg mr-5 font-bold">
+            <div className="flex items-center">
+              <span className="mr-5">槓桿倍數:</span>
+              <Typography id="discrete-slider-custom">{`leverage: ${slideValue}x`}</Typography>
+            </div>
+            <div>
+              {/* {console.log('L:', _.get(account, 'result.leverage', 3), 'X;', slideValue)} */}
+              <Slider
+                style={{ width: 300 }}
+                value={slideValue}
+                onChange={handleChangeSlide}
+                aria-labelledby="discrete-slider-custom"
+                step={1}
+                valueLabelDisplay="auto"
+                marks={leverageMarks}
+              />
+            </div>
+          </span>
+        </div>
 
         {/* 開倉參數 */}
         <Open />
